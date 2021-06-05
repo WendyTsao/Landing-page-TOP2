7ca5b4988c475210a1c30e90141c07e4b642e937
Wendy <wendytsao84@gmail.com>
2021-06-05 17:03:34 +0800

    extract Graphic components

---
+++ src/components/Graphic.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="col-12 col-lg-6">
+    <slot></slot>
+    <img :src="require(`@/assets/pic/${image}`)" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
