f49515ea3ddf8f87a22d8c2646cf3082a57ce7ae
Wendy <wendytsao84@gmail.com>
2021-06-06 17:55:17 +0800

    npm AOS

--- src/main.js
+++ src/main.js
@@ -8,3 +8,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js//bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './assets/css/style.css';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
+AOS.init();
